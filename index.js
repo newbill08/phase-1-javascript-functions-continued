42566a17a7c2aa7b798def6b3e7c216a96b52658
joshua <josh.newbill08@hotmail.com>
2023-05-19 17:45:08 -0500

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(argument='roller-skate'){
+
+return `This Saturday, I want to ${argument}!`
+}
+
+
+function mondayWork(Argument ='go to the office'){
+    return `This Monday, I will ${Argument}.`
+}
+
+function wrapAdjective(flair ="*" ){
+    return function (Argument= "special" ){ return `You are ${flair}${Argument}${flair}!`
+}
+}
